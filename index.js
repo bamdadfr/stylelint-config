6cb5d6b820f34a1919e2ab8742d5c9550ffc86b3
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-05-21 17:03:48 +0200

    commit before publish

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  'overrides': [
-    {
-      'extends': ['./config.js'],
-      'files': ['*.js', '*.jsx', '*.ts', '*.tsx']
+    "extends": [
+        "stylelint-config-recommended",
+        "stylelint-config-styled-components"
+    ],
+    "rules": {
+        "indentation": 4,
+        "max-empty-lines": 1,
+        "at-rule-empty-line-before": [ "always" ],
+        "block-closing-brace-empty-line-before": [ "never" ],
+        "custom-property-empty-line-before": [ "always" ],
+        "comment-empty-line-before":
+        [
+            "always",
+            {
+                "except": [ "first-nested" ]
+            }
+        ],
+        "declaration-empty-line-before":
+        [
+            "always",
+            {
+                "except": [ "after-declaration", "first-nested" ]
+            }
+        ],
+        "rule-empty-line-before":
+        [
+            "always",
+            {
+                "except": [ "first-nested" ],
+            }
+        ]
     }
-  ]
-};
+}
