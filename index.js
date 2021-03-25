f073fc3bea6870f5607f3f78e225ccfbe5f76d03
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-03-26 00:51:46 +0100

    feat(rules): more flexible [force-release]

--- index.js
+++ index.js
@@ -7,10 +7,23 @@ module.exports = {
     ],
     "rules": {
         "indentation": 4,
-        "max-empty-lines": 1,
-        "at-rule-empty-line-before": [ "always" ],
-        "block-closing-brace-empty-line-before": [ "never" ],
-        "custom-property-empty-line-before": [ "always" ],
+        // "max-empty-lines": 1,
+        // "at-rule-empty-line-before": "never",
+        // "block-closing-brace-empty-line-before": "never",
+        "no-descending-specificity": null,
+        "at-rule-no-unknown": [
+            true,
+            {
+                ignoreAtRules: [
+                    "function",
+                    "if",
+                    "each",
+                    "include",
+                    "mixin"
+                ]
+            }
+        ],
+        "custom-property-empty-line-before": [ "never" ],
         "comment-empty-line-before":
         [
             "always",
